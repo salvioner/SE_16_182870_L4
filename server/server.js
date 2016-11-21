9ad0be273c521a485eb37011d9335511d113fc19
salvioner <arighi.a@gmail.com>
2016-11-22 00:55:04 +0100

    node.js server initial setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 // THIS SERVER USES NODE.JS
+// All required packages have already been downloaded in ./node_modules
+
+// required packages
+var http = require('http');
+var util = require('util');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+
+// POST
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//JSON POST
+app.use(bodyParser.json());
+
+// specifying access port
+app.set('port', (process.env.PORT || 1337));
